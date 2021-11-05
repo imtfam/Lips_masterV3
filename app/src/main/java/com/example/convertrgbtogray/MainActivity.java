105f87fc99a53efd40d83da1ca9b69e9211fa4e0
imtfam <mini-fam@hotmail.com>
2021-11-05 20:44:26 +0700

    Update 5/11/2564 20.44

--- app/src/main/java/com/example/convertrgbtogray/MainActivity.java
+++ app/src/main/java/com/example/convertrgbtogray/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageButton btnHome,btnCameraHome;
+    public static final int REQUEST_GALLERY = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +27,14 @@ public class MainActivity extends AppCompatActivity {
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Process.class);
-                startActivity(intent);
+
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        intent.setType("image/*");
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_GALLERY);
+
+
+               /* Intent intent = new Intent(getApplicationContext(), Process.class);
+                startActivity(intent);*/
             }
 
         });
@@ -32,5 +46,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent2);
             }
         });
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_GALLERY && resultCode == RESULT_OK) {
+                Uri uri = data.getData();
+                Intent intent = new Intent(MainActivity.this, Process.class);
+                intent.putExtra("imageGallery",uri);
+                startActivity(intent);
+
+        }
     }
 }
